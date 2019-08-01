edd34a711f4f4cff20877305941576799aedb998
JacksonTom <JacksonTom@qq.com>
2019-08-01 10:38:01 +0800

    create parent

---
+++ parent/src/main/java/com/github/jacksontom/parent/App.java
@@ -0,0 +1,13 @@
+package com.github.jacksontom.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
